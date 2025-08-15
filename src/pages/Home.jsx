68819cd7ff67495245289c9db33404b1686e919d
kevinsurya123 <kevinsurya1212@gmail.com>
2025-08-16 02:14:33 +0700

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { ThemeTogle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return ( 
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/* Theme Toggle */}
+            <ThemeTogle />
+        {/* Background Effect */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+
+        {/*Footer */}
+
+
+    </div>
+    );
+};
